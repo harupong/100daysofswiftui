b4ab26548814417432df02c14320fbe33d75e020
harupong <harupong@gmail.com>
2023-05-26 18:18:20 +0900

    WIP day8 completed up to Summary:Functions
    https://www.hackingwithswift.com/quick-start/beginners/summary-functions

--- 100daysofswiftui.playground/Contents.swift
+++ 100daysofswiftui.playground/Contents.swift
@@ -1,1 +1,52 @@
 import Cocoa
+
+//day8 default parameter
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+var array = [1, 2, 3]
+array.removeAll()
+array.removeAll(keepingCapacity: true)
+
+//error handling
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let string = "123"
+
+//do and try must come togetther
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
