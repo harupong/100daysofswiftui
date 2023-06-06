cc25566e46229305981ccf5e0d86d5d20d388ae4
harupong <harupong@gmail.com>
2023-06-06 09:25:53 +0900

    complete day013

---
+++ day013.playground/Contents.swift
@@ -0,0 +1,174 @@
+import Cocoa
+
+//day13 protocols
+protocol Vehicle {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+func commute(distance: Int, using vehicle: Vehicle) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
+    }
+}
+
+
+struct Car: Vehicle {
+    let name = "Car"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
+    }
+
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km.")
+    }
+
+    func openSunroof() {
+        print("It's a nice day!")
+    }
+}
+
+struct Bicycle: Vehicle {
+    let name = "Bicycle"
+    var currentPassengers = 1
+    
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km.")
+    }
+}
+
+let bike = Bicycle()
+commute(distance: 50, using: bike)
+
+let car = Car()
+commute(distance: 200, using: car)
+
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    }
+}
+
+getTravelEstimates(using: [car, bike], distance: 150)
+
+//opaque return types
+//by using "some", you can hide what type is actually returned.
+//This is really useful in SwiftUI as, for example, you can avoid specifing all the views being returned.
+func getRandomNumber() -> some Equatable {
+    Int.random(in: 1...6)
+}
+
+func getRandomBool() -> some Equatable {
+    Bool.random()
+}
+
+//extensions
+var quote = "   The truth is rarely pure and never simple   "
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+extension String {
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+    
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    mutating func trim() {
+        self = self.trimmed()
+    }
+}
+
+let trimmed2 = quote.trimmed()
+trimmed == trimmed2
+
+quote.trim()
+trimmed == quote
+
+let lyrics = """
+But I keep cruising
+Can't stop, won't stop moving
+It's like I got this music in my mind
+Saying it's gonna be alright
+"""
+
+print(lyrics.lines.count)
+print(lyrics.lines)
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+
+//by defining initializer using extension, you can make both custom and memberwise initializers work
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let andy1 = Book(title: "The Martians", pageCount: 720)
+let andy2 = Book(title: "Project Hail mary", pageCount: 1200, readingHours: 30)
+
+//protocol extensions
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+let drivers = ["tsunoda", "sainz", "alonso"]
+if drivers.isNotEmpty {
+    print("Drivers count: \(drivers.count)")
+}
+
+//checkpoint8
+protocol Building {
+    var rooms: Int { get }
+    var cost: Int { get }
+    var agent: String { get }
+    func printSalesSummary()
+}
+
+struct House: Building {
+    let rooms: Int
+    let cost: Int
+    let agent: String
+    func printSalesSummary() {
+        print("This house has \(rooms) rooms and is for sale at \(cost) yen.")
+        print("If you're interested please contact \(agent) for details.")
+    }
+}
+
+struct Office: Building {
+    let rooms: Int
+    let cost: Int
+    let agent: String
+    func printSalesSummary() {
+        print("This house has \(rooms) rooms and is for sale at \(cost) yen.")
+        print("If you're interested please contact \(agent) for details.")
+    }
+}
+
+let myHouse = House(rooms: 3, cost: 10_000_000, agent: "Aoyama Real Estate")
+myHouse.printSalesSummary()
+
+let myOffice = Office(rooms: 20, cost: 100_000_000, agent: "Office Real Estate")
+myOffice.printSalesSummary()
